52cf2a0c2590ebec9ea98e1b53ed65991b11e855
Obada Ajaj <top1obada@gmail.com>
2025-11-18 05:29:56 +0300

    Update Settings.cs

--- ATOZDataLayer/Settings/Settings.cs
+++ ATOZDataLayer/Settings/Settings.cs
@@ -9,10 +9,9 @@ namespace ATOZDataLayer.Settings
     public static class clsSettings
     {
 
-        //"Server=.;Database=ATOZDB;User Id=sa;Password=sa123456;TrustServerCertificate=True;";
-        //Server=db28092.databaseasp.net; Database=db28092; User Id=db28092; Password=S=i76X@r?hW3; Encrypt=False; MultipleActiveResultSets=True;
+        
         public static string ConnectionString =
-            "Server=db28092.public.databaseasp.net; Database=db28092; User Id=db28092; Password=S=i76X@r?hW3; Encrypt=False; MultipleActiveResultSets=True";
+            "connection string here";
 
 
 
